c5b0c66448b18cc0bf7d8a5a6e3929fd34d58cf4
Beckylee <Beckylee.Dell@tufts.edu>
2015-07-09 18:02:47 -0400

    cleaned up the code, changed the error message

--- guess.js
+++ guess.js
@@ -2,8 +2,9 @@
 $(document).ready(function(){
 	var number;
 	var num_guesses;
-	var prev_guesses = [];
-	var guesses_text = "";
+	var prev_guesses;
+	var guesses_text;
+
 	reset();
 
 	$('.center-block').on('click', 'button', guess_num);
@@ -22,60 +23,59 @@ $(document).ready(function(){
 
 	function guess_num(){
 		var jumbo = $(this).closest('.jumbotron');
-		var warning = jumbo.find('.warning');
+		var warning = jumbo.find('.help-block');
 		var guess = jumbo.find('#guess');
 		var input = +guess.val();
-		guess.val('');
-		var hotness = '';
 		var direction = '';
-		var repeat = false;
-		guess.css('border-color','#4cae4c');
-		warning.text('');
+
+console.log(number);
+		cleanup();
 
 		//test to make sure that the number is a number between 0 and 100
 		if(input>0 && input <100){
+
 			//check to see if input is a repeat
 			for(var i = 0; i<prev_guesses.length; i++){
 				if(prev_guesses[i]==input){
-					repeat=true;
+					warning.text("You've already guessed that number.  Please pick a different one.");
+					return null;
 				}
 			}
 
-			if(repeat == true){
-				warning.text("You've already guessed that number.  Please pick a different one.");
+
+			//update the number of guesses left
+			$('#count').text(num_guesses);
+
+			//check to see if the number is right
+			if(input == number){
+				$('.setup').hide();
+				jumbo.find('.win').html("<h1>" + number + "<br />You win!!</h1>");
+				$('body').addClass('winner');
 			}else{
-				$('#count').text(num_guesses);
-				//check to see if the number is right
-				if(input == number){
-					$('.setup').hide();
-					jumbo.find('.win').html("<h1>" + number + "<br />You win!!</h1>");
-					$('body').addClass('winner');
+				//check if input is higher or lower than the number
+				if(input>number){
+					direction = 'lower';
 				}else{
-					//check if input is higher or lower than the number
-					if(input>number){
-						direction = 'lower';
-					}else{
-						direction = 'higher';
-					}
+					direction = 'higher';
+				}
 
-					hotness = howfar(input);
-
-					jumbo.find('.how_close').text('You are '+ hotness + '. Guess '+ direction);
-					num_guesses = num_guesses-1;
-					prev_guesses.push(input);
-					$('.guesses').html(guesses_text);
+				//tell the player how they're doing.
+				jumbo.find('.how_close').text("You're " + howfar(input) + '. Guess '+ direction);
+				
+				num_guesses--;
+				prev_guesses.push(input);
+				$('.guesses').html(guesses_text);
 
 
-					if(num_guesses == -1){
-						gameover();
-					}
+				if(num_guesses == -1){
+					gameover();
 				}
 			}
 
 		}else{
 			//if the entry isn't a number between 1 and 100, change the input box border to red and 
 			//show a warning
-			guess.css('border-color','red');
+			guess.parent().addClass('has-error');
 			warning.text('Please enter a valid number between 1 and 100');
 		}
 		
@@ -85,16 +85,17 @@ $(document).ready(function(){
 
 	function howfar(input){
 		var diff = Math.abs(input-number);
-
-		
 		var prev_diff = Math.abs(prev_guesses[prev_guesses.length-1] - number);
 
+		//assign hot/red to input number depending on if it's closer than the previous guess
+		//or if it's within 40 of the number (if it's the first guess)
+		if((diff<prev_diff)||(isNaN(prev_diff)&& diff<40)){
+			guesses_text += "<span class='hot'>" + input + "</span>, ";
+		}else{
+			guesses_text += "<span class='cold'>" + input + "</span>, ";
+		}
+
 		if(prev_guesses.length == 0){
-			if(diff>40){
-				guesses_text += "<span class='cold'>" + input + "</span>, ";
-			}else{
-				guesses_text += "<span class='hot'>" + input + "</span>, ";
-			}
 
 			if(diff<10){
 				return 'on fire';
@@ -115,10 +116,8 @@ $(document).ready(function(){
 			}
 		}else{
 			if(diff<prev_diff){
-				guesses_text += "<span class='hot'>" + input + "</span>, ";
 				return 'getting warmer!';
 			}else{
-				guesses_text += "<span class='cold'>" + input + "</span>, ";
 				return 'getting colder :(';
 			}
 		}
@@ -136,17 +135,21 @@ $(document).ready(function(){
 
 	//reset or start a new game
 	function reset(){
-		$('body').removeClass('winner');
-		number = Math.floor(Math.random()*100);
+		number = Math.floor(Math.random()*100)+1;
 		num_guesses = 4;
 		prev_guesses = [];
-		guesses_text = "";
-		$('.win').text('');
-		$('.guesses').html(guesses_text);
+		guesses_text = "Previous guesses: ";
 
+		$('body').removeClass('winner');
+		$('.blankstart').text('');
 		$('.setup').show();
-		$('.how_close').text('');
 		$('#count').text('5');
+	}
+
+	function cleanup(){
 		$('#guess').val('');
+		$('.help-block').text('');
+		$('#guess').parent().removeClass('has-error');
+
 	}
 });
